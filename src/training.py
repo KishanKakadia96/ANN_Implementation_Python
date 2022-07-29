93bd5bec04e1995156edc70a876c67978df3b5b8
KishanKakadia96 <kishankakadia96@gmail.com>
2022-07-29 11:08:10 +0200

    imports from model.py to Create and save model

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 import os
 from src.utils.common import read_config  
 from src.utils.data_management import get_data
+from src.utils.model import create_model,save_model
 import argparse
 
 # os.chdir('C:\\Kishan\\Github\\Artificial_Neural_Network\\ANN_Implementation_Python')
@@ -11,6 +12,26 @@ def training(config_path):
     validation_datasize = config["params"]["validation_datasize"]
     
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
+
+    LOSS_FUNCTION = config["params"]["loss_functions"]
+    OPTIMIZER = config["params"]["optimizer"]   
+    METRICS = config["params"]["metrics"]
+    NUM_CLASSES = config["params"]["num_classes"]
+
+    model = create_model(LOSS_FUNCTION,OPTIMIZER,METRICS, NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"] 
+    VALIDATION = (X_valid, y_valid)
+
+    history= model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+
+    artifacts_dir= config["artifacts"]["artifacts_dir"]
+    model_dir= config["artifacts"]["model_dir"]
+    model_dir_path = os.path.join(artifacts_dir,model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model,model_name,model_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
