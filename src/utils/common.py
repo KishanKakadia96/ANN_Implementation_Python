226202ef5259a5154e4b5904e3bb0dd12fc13af0
KishanKakadia96 <kishankakadia96@gmail.com>
2022-07-30 12:12:58 +0200

    make timestamp function

--- src/utils/common.py
+++ src/utils/common.py
@@ -5,3 +5,9 @@ def read_config(config_path):
         content = yaml.safe_load(config_file)
 
     return content
+
+def get_timestamp(name):
+    timestamp = time.asctime().replace(" ", "_").replace(":", "_")
+    unique_name = f"{name}_at_{timestamp}" 
+
+    return unique_name
